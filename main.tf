f680b616b8cac38df7d0721ae37c144c709df0c0
Sofiane Kihal <sofianekihal.pro@gmail.com>
2022-10-11 18:03:15 +0200

    Add location

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "4.40.0"
     }
   }
 }
@@ -16,4 +16,5 @@ provider "google" {
 
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-opeopenomi-1"
+  location= var.bucket_location
 }
