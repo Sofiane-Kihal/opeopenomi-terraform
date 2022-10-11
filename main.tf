aeda9f78ded907960f16193a01a527acc38a01e5
Sofiane Kihal <37071742+Sofiane-Kihal@users.noreply.github.com>
2022-10-11 17:18:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = var.project_id
+  region  = var.region
+}
+
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-opeopenomi-1"
 }
