e039d3074315828e2bfafcaa6a2f4dbf457b67b5
Sofiane Kihal <sofianekihal.pro@gmail.com>
2022-10-12 14:14:18 +0200

    Adding github secrets env var for credentials

--- providers.tf
+++ providers.tf
@@ -6,17 +6,8 @@ terraform {
     }
   }
 
-  cloud {
-  organization = "opeopenomi"
-
-  workspaces {
-    name = "gcp-actions-demo"
-  }
-}
-
 }
 provider "google" {
-  credentials = var.GOOGLE_CREDENTIALS
 
   project = var.project_id
   region  = var.region
